f2c1d49b2f6c59439826a8b18406bd388b39d1a6
Will <16578982+CloudG360@users.noreply.github.com>
2021-06-05 20:25:02 +0100

    Added . to allowed statisticID characters

--- src/main/java/org/madblock/lib/stattrack/util/Verify.java
+++ src/main/java/org/madblock/lib/stattrack/util/Verify.java
@@ -3,11 +3,11 @@ package org.madblock.lib.stattrack.util;
 public class Verify {
 
     public static boolean statisticID(String id) {
-        return id.matches("([A-Za-z_]*)");
+        return id.matches("([A-Za-z_.]*)");
     }
 
     public static String andCorrectStatisticID(String id) {
-        return id.replaceAll("([^A-Za-z_]+)", "_").toLowerCase();
+        return id.replaceAll("([^A-Za-z_.]+)", "_").toLowerCase();
     }
 
 }
